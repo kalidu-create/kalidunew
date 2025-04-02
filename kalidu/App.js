b3a919ab5c17e8946d88bfa07bbc7adfd47b3647
Kalidu-create <kalidunirmal99@gmail.com>
2025-04-02 11:52:49 +0530

    add folder

---
+++ kalidu/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from './header';
+import About from './about';
+
+function App() {
+  return (
+<div className='App'>
+
+<Header/>
+<About/>
+
+
+
+
+
+</div>
+
+  );
+  
+
+}
+
+export default App;
